e029c893ce67a867f3c04d79014e3669ebd2d3cb
Valeria Cortez <valeriacortez@ucmerced-10-34-100-134.ucmerced.edu>
2019-11-17 17:51:13 -0800

    I wrote a code for computer choice, the rock, paper, scissors is determined by a random time.

--- App.js
+++ App.js
@@ -6,5 +6,24 @@ const modal = document.querySelector('.modal');
 const scoreboard = {
     player: 0,
     computer: 0
+};
+
+function play(e) {
+    restart.style.display = 'inline-block';
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
 }
+
+function getComputerChoice() {
+    const rand = Math.random();
+    if(rand < 0.34) {
+        return 'rock';
+    } else if(rand <= 0.57) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+
 
